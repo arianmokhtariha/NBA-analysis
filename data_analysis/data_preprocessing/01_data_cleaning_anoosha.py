fe006d4bb6375ff32157bbd8e4ae127ebe6428b0
anooshanth <70814715+anooshanth@users.noreply.github.com>
2025-09-25 18:48:34 +0330

    added data cleaning pipeline for players table

--- data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
+++ data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
@@ -1,18 +1,54 @@
-
 import pandas as pd
 import os
 
-players_raw_path = os.path.join(os.path.dirname(__file__),'..','..','data','Players_table.csv')
-players_raw_df = pd.read_csv(players_raw_path)
+#####-------cleaning players data--------#######
+players_raw_path = os.path.join(
+    os.path.dirname(__file__), "..", "..", "data", "Players_table.csv"
+)
+players_df = pd.read_csv(players_raw_path, index_col=0)
 
-def split_columns():
-    pass
+#lower case columns
+players_df.columns = players_df.columns.str.lower()
 
-def inch_to_cm ():
-    pass
+##expand and strip positions column
+players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
+    "position"
+].str.split(",", expand=True)
+players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
+    ["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]
+].map(lambda x: x.strip() if isinstance(x, str) else x)
 
-def age():
-    pass
+###change height to cms
+height_split = players_df["height"].str.strip().str.split("-", expand=True)
+
+players_df["height_in_cm"] = (height_split[0].astype(float) * 30.48) + (
+    height_split[1].astype(float) * 2.54
+)
+players_df["height_in_cm"] = players_df["height_in_cm"].round()
+
+###change weight to kgs
+players_df["weight_in_kg"] = (players_df["weight"] * 0.453).round()
+
+###create an age column based on birth year
+players_df["age"] = 2025 - players_df["birthyear"]
 
 
+##drop unnecessary columns (important for Nf1, Nf2, Nf3)
+players_df = players_df.drop(
+    columns=[
+        "position",
+        "height",
+        "weight",
+        "birthday",
+        "birthmonth",
+        "birthyear",
+    ]
+)
 
+###save it to csv
+players_df.to_csv(
+    os.path.join(
+        os.path.dirname(__file__), "..", "..", "data", "data_clean", "players_clean.csv"
+    ),
+    index=False,
+)
