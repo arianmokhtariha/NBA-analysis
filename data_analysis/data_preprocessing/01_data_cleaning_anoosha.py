d3d760e9e7306663fc6b5d03d86b4fd10885c806
anooshanth <70814715+anooshanth@users.noreply.github.com>
2025-09-25 17:53:05 +0330

    folder structure

---
+++ data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import os
+
+players_raw_path = os.path.join(os.path.dirname(__file__),'..','..','data','Players_table.csv')
+players_raw_df = pd.read_csv(players_raw_path)
+
+def split_columns():
+    pass
+
+def inch_to_cm ():
+    pass
+
+def age():
+    pass
+
+
+
