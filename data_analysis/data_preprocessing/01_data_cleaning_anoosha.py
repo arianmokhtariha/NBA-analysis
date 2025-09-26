655c2801cc484761dff66d591b7d37db6de64a1b
anooshanth <70814715+anooshanth@users.noreply.github.com>
2025-09-26 16:48:02 +0330

    clean up player stats + mvp candidates + mvp winners

--- data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
+++ data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
@@ -2,56 +2,57 @@ import pandas as pd
 import os
 
 #####-------cleaning players data--------#######
-players_raw_path = os.path.join(
+def clean_players():
+    players_raw_path = os.path.join(
     os.path.dirname(__file__), "..", "..", "data", "Players_table.csv"
 )
-players_df = pd.read_csv(players_raw_path, index_col=0)
+    players_df = pd.read_csv(players_raw_path, index_col=0)
 
-# lower case columns
-players_df.columns = players_df.columns.str.lower()
+        # lower case columns
+        players_df.columns = players_df.columns.str.lower()
 
-##expand and strip positions column
-players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
-    "position"
-].str.split(",", expand=True)
-players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
-    ["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]
-].map(lambda x: x.strip() if isinstance(x, str) else x)
+        ##expand and strip positions column
+        players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
+            "position"
+        ].str.split(",", expand=True)
+        players_df[["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]] = players_df[
+            ["pos1", "pos2", "pos3", "pos4", "pos5", "pos6"]
+        ].map(lambda x: x.strip() if isinstance(x, str) else x)
 
-###change height to cms
-height_split = players_df["height"].str.strip().str.split("-", expand=True)
+        ###change height to cms
+        height_split = players_df["height"].str.strip().str.split("-", expand=True)
 
-players_df["height_in_cm"] = (height_split[0].astype(float) * 30.48) + (
-    height_split[1].astype(float) * 2.54
-)
-players_df["height_in_cm"] = players_df["height_in_cm"].round()
+        players_df["height_in_cm"] = (height_split[0].astype(float) * 30.48) + (
+            height_split[1].astype(float) * 2.54
+        )
+        players_df["height_in_cm"] = players_df["height_in_cm"].round()
 
-###change weight to kgs
-players_df["weight_in_kg"] = (players_df["weight"] * 0.453).round()
+        ###change weight to kgs
+        players_df["weight_in_kg"] = (players_df["weight"] * 0.453).round()
 
-###create an age column based on birth year
-players_df["age"] = 2025 - players_df["birthyear"]
+        ###create an age column based on birth year
+        players_df["age"] = 2025 - players_df["birthyear"]
 
 
-##drop unnecessary columns (important for Nf1, Nf2, Nf3)
-players_df = players_df.drop(
-    columns=[
-        "position",
-        "height",
-        "weight",
-        "birthday",
-        "birthmonth",
-        "birthyear",
-    ]
-)
+        ##drop unnecessary columns (important for Nf1, Nf2, Nf3)
+        players_df = players_df.drop(
+            columns=[
+                "position",
+                "height",
+                "weight",
+                "birthday",
+                "birthmonth",
+                "birthyear",
+            ]
+        )
 
-###save it to csv
-players_df.to_csv(
-    os.path.join(
-        os.path.dirname(__file__), "..", "..", "data", "data_clean", "players.csv"
-    ),
-    index=False,
-)
+        ###save it to csv
+        players_df.to_csv(
+            os.path.join(
+                os.path.dirname(__file__), "..", "..", "data", "data_clean", "players.csv"
+            ),
+            index=False,
+        )
 
 
 ##################-------------cleaning Mvp Candidates-----------------######################################
@@ -122,5 +123,116 @@ mvp_candidates_df.to_csv(
         "data",
         "data_clean",
         "mvp_candidates.csv",
-    )
+    ),
+    index=False
 )
+
+
+
+#####-----cleaning mvp winners-----############
+
+mvp_winners_raw_path = os.path.join(
+    os.path.dirname(__file__), "..", "..", "data", "Mvp_table.csv"
+)
+
+mvp_winners_df = pd.read_csv(mvp_winners_raw_path, index_col=0)
+
+mvp_winners_df.columns = mvp_winners_df.columns.str.lower().str.strip()
+
+mvp_winners_column_map = {
+    "season": "season",
+    "player_id": "player_id",
+    "age": "age",
+    "team_id": "team_id",
+    "game": "games",
+    "minutes played per game": "minutes_per_game",
+    "points per game": "points_per_game",
+    "total rebounds per game": "rebounds_per_game",
+    "assists per game": "assists_per_game",
+    "steals per game": "steals_per_game",
+    "blocks per game": "blocks_per_game",
+    "field goal percentage": "field_goal_pct",
+    "3-point field goal percentage": "three_point_pct",
+    "free throw percentage": "free_throw_pct",
+    "win shares": "win_shares",
+    "win shares per 48 minutes": "win_shares_per_48",
+}
+mvp_winners_df.rename(columns=mvp_winners_column_map, inplace=True)
+
+split_year_mvpwinners = mvp_winners_df["season"].str.split("-", expand=True)
+split_year_mvpwinners[0] = split_year_mvpwinners[0].astype(int) + 1
+mvp_winners_df["season"] = split_year_mvpwinners[0].values
+
+mvp_winners_df.to_csv(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+        "data",
+        "data_clean",
+        "mvp_winners.csv"
+    ), index= False
+)
+
+###########------cleaning player stats----------------###########
+
+player_stats_raw_path = os.path.join(
+    os.path.dirname(__file__), "..", "..", "data", "player_stats.xlsx"
+)
+player_stats = pd.read_excel(player_stats_raw_path)
+
+player_stats.columns = player_stats.columns.str.lower()
+
+player_stats_column_map = {
+    "year": "season",
+    "rank": "rank",
+    "player_id": "player_id",
+    "age": "age",
+    "team": "team_id",
+    "position": "position",
+    "games": "games_played",
+    "games started": "games_started",
+    "minutes played": "minutes_played",
+    "fg": "field_goals_made",
+    "fga": "field_goals_attempted",
+    "fg%": "field_goal_pct",
+    "3p": "three_pointers_made",
+    "3pa": "three_pointers_attempted",
+    "3p%": "three_point_pct",
+    "2p": "two_pointers_made",
+    "2pa": "two_pointers_attempted",
+    "2p%": "two_point_pct",
+    "efg%": "effective_fg_pct",
+    "ft": "free_throws_made",
+    "fta": "free_throws_attempted",
+    "ft%": "free_throw_pct",
+    "orb": "offensive_rebounds",
+    "drb": "defensive_rebounds",
+    "trb": "total_rebounds",
+    "ast": "assists",
+    "stl": "steals",
+    "blk": "blocks",
+    "tov": "turnovers",
+    "pf": "personal_fouls",
+    "pts": "points",
+    "trp_dbl": "triple_doubles",
+}
+
+player_stats = player_stats.rename(columns=player_stats_column_map)
+
+print (player_stats[player_stats[["season", "player_id"]].duplicated()])
+print (player_stats.isna().sum())
+
+player_stats["team_id"] = player_stats["team_id"].fillna("TOT")
+
+player_stats = player_stats[player_stats["team_id"] == "TOT"].copy()
+
+
+
+player_stats.to_csv(
+    os.path.join(
+        os.path.dirname(__file__), "..", "..", "data", "data_clean", "player_stats.csv"
+    ),
+    index=False,
+)
+
