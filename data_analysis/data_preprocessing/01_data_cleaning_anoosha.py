3637321c05f8d936ca02647bb06ed43528a5f7cb
anooshanth <70814715+anooshanth@users.noreply.github.com>
2025-09-26 13:29:32 +0330

    mvp candidates clean up + removed unused csvs

--- data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
+++ data_analysis/data_preprocessing/01_data_cleaning_anoosha.py
@@ -7,7 +7,7 @@ players_raw_path = os.path.join(
 )
 players_df = pd.read_csv(players_raw_path, index_col=0)
 
-#lower case columns
+# lower case columns
 players_df.columns = players_df.columns.str.lower()
 
 ##expand and strip positions column
@@ -48,7 +48,79 @@ players_df = players_df.drop(
 ###save it to csv
 players_df.to_csv(
     os.path.join(
-        os.path.dirname(__file__), "..", "..", "data", "data_clean", "players_clean.csv"
+        os.path.dirname(__file__), "..", "..", "data", "data_clean", "players.csv"
     ),
     index=False,
+)
+
+
+##################-------------cleaning Mvp Candidates-----------------######################################
+
+mvp_candidates_raw_path = os.path.join(
+    os.path.dirname(__file__), "..", "..", "data", "new_mvp_candidates.xlsx"
+)
+mvp_candidates_df = pd.read_excel(mvp_candidates_raw_path)
+
+mvp_candidates_df.columns = mvp_candidates_df.columns.str.lower()
+
+mvp_candidates_df_col_names = {
+    "first_place_vote": "first_place_votes",
+    "pts won": "points_won",
+    "pts max": "points_max",
+    "fg%": "fg_pct",
+    "3p%": "three_pct",
+    "ft%": "ft_pct",
+    "ws/48": "ws_per_48",
+}
+
+mvp_candidates_df = mvp_candidates_df.rename(columns=mvp_candidates_df_col_names)
+
+print(mvp_candidates_df[["year", "player_id"]].duplicated().sum())
+### this should be zero, because this pair will be our composite key ##
+
+split_t = mvp_candidates_df["rank"].str.strip().str.extract(r"(\d+)(T?)")
+split_t[1] = split_t[1].astype(bool)
+
+mvp_candidates_df[["rank", "tie"]] = split_t[[0, 1]].values
+
+mvp_candidates_df_col_order = [
+    "year",
+    "player_id",
+    "rank",
+    "tie",
+    "age",
+    "team",
+    "first_place_votes",
+    "points_won",
+    "points_max",
+    "share",
+    "games",
+    "mp",
+    "pts",
+    "trb",
+    "ast",
+    "stl",
+    "blk",
+    "fg_pct",
+    "three_pct",
+    "ft_pct",
+    "ws",
+    "ws_per_48",
+]
+
+mvp_candidates_df = mvp_candidates_df[mvp_candidates_df_col_order]
+
+mvp_candidates_df["rank"] = mvp_candidates_df["rank"].astype("int64")
+mvp_candidates_df["tie"] = mvp_candidates_df["tie"].astype(bool)
+
+
+mvp_candidates_df.to_csv(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+        "data",
+        "data_clean",
+        "mvp_candidates.csv",
+    )
 )
